3f3999ad52e9e8d288e76626f685881240e8fd7d
jonathanengelbert <jonathanengelbert@gmail.com>
2020-07-21 10:40:59 -0400

    Revamps Footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 
 import './footerStyles.scss';
+import Navbar from 'react-bootstrap/esm/Navbar';
 
 export default function Footer() {
     return (
         <footer id={"footer"}>
-            <strong>
-                <p>
-                    © 2020 Jonathan Engelbert
-                </p>
-            </strong>
+            <Navbar id="footer-bar" bg="dark" expand="lg" sticky-bottom>
+                <strong>
+                    <p>
+                        <a href="tel:+1-555-555-5555">+1-555-555-5555</a>
+                    </p>
+                    <p>
+                        minervaguitars@nowhere.com
+                    </p>
+                    <p>
+                        © 2020 Jonathan Engelbert
+                    </p>
+                </strong>
+            </Navbar>
         </footer>
     )
 }
