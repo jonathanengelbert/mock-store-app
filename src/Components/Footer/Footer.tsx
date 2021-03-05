5ae06f960fa5852c961dab48d8b043335d20d44e
Jon Engelbert <jonengelbert@kinsta.com>
2021-03-05 11:40:23 -0500

    Runs audit with force

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,27 +1,21 @@
-import React from 'react';
+import React from 'react'
 
-import './footerStyles.scss';
-import Navbar from 'react-bootstrap/esm/Navbar';
+import './footerStyles.scss'
+import Navbar from 'react-bootstrap/esm/Navbar'
 
 export default function Footer() {
-    return (
-        <footer id={"footer"}>
-            <Navbar id="footer-bar" bg="dark" expand="lg" sticky={"bottom"}>
-                <strong>
-                    <p>
-                        <a href="tel:+1-555-555-5555">+1-555-555-5555</a>
-                    </p>
-                    <p>
-                        minervaguitars@nowhere.com
-                    </p>
-                    <p>
-                        555 Avenue X, Nowhere Town, NW, Nowhere Land
-                    </p>
-                    <p>
-                        © 2020 Jonathan Engelbert
-                    </p>
-                </strong>
-            </Navbar>
-        </footer>
-    )
+  return (
+    <footer id={'footer'}>
+      <Navbar id="footer-bar" bg="dark" expand="lg">
+        <strong>
+          <p>
+            <a href="tel:+1-555-555-5555">+1-555-555-5555</a>
+          </p>
+          <p>minervaguitars@nowhere.com</p>
+          <p>555 Avenue X, Nowhere Town, NW, Nowhere Land</p>
+          <p>© 2020 Jonathan Engelbert</p>
+        </strong>
+      </Navbar>
+    </footer>
+  )
 }
